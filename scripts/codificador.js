01214fe974f10c540751a65e67ba54a8ac08a232
levimaciell <levimaciell1221@gmail.com>
2024-02-04 17:07:07 -0300

    setting up basics

---
+++ scripts/codificador.js
@@ -0,0 +1,18 @@
+// A letra "e" é convertida para "enter"
+// A letra "i" é convertida para "imes"
+// A letra "a" é convertida para "ai"
+// A letra "o" é convertida para "ober"
+// A letra "u" é convertida para "ufat"
+
+//Requisitos:
+// Deve funcionar apenas com letras minúsculas
+// Não devem ser utilizados letras com acentos nem caracteres especiais
+// Deve ser possível converter uma palavra para a versão criptografada e também retornar uma palavra criptografada para a versão original.
+
+function codificar(){
+    console.log("Codificou :)");
+}
+
+function decodificar(){
+    console.log("Decodificou :)");
+}
