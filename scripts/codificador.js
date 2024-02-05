85d53f789c53288dbe1a68db1394bcee7e9ef944
levimaciell <levimaciell1221@gmail.com>
2024-02-05 09:58:19 -0300

    Encode function working

--- scripts/codificador.js
+++ scripts/codificador.js
@@ -9,11 +9,31 @@
 // Não devem ser utilizados letras com acentos nem caracteres especiais
 // Deve ser possível converter uma palavra para a versão criptografada e também retornar uma palavra criptografada para a versão original.
 
+const map = new Map([
+    ["e", "enter"],
+    ["i", "imes"],
+    ["a", "ai"],
+    ["o", "ober"],
+    ["u", "ufat"]
+])
+
 function codificar(){
+    
     let campo = document.getElementById("campo");
     let texto = campo.value;
+    actual = "";
 
-    console.log(texto)
+    for (let i = 0; i < texto.length; i++) {
+
+        if(map.get(texto[i]) != undefined){
+            actual += map.get(texto[i]);
+        }
+        else{
+            actual += texto[i];
+        }
+    }
+
+    campo.value = actual
 }
 
 function decodificar(){
